f37b7d22455de95de5e5c6e40dbf88b6c241baa9
AhmedGit-world <ahmed.5250@outlook.com>
2025-06-09 21:41:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,1 @@
-provider "aws" {
-  region     = "us-east-1"
-}
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-jenkins-terraform-s3-bucket-12345"
-  tags = {
-    Name = "JenkinsTerraformBucket"
-  }
-}
